98ec66ac8b9373e871a31543cfac93b55c3f2487
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:50:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,5 +23,7 @@ def swap_elements_from_to(array, starting, ending)
   switched_array = Array.new
   spaces = ending - starting
   switched_array = array[0...starting - 1] + [array[ending]] + array[starting + 1..ending - 1] + [array[starting]] + array[starting..array.length - 1]
+  switched_array
+end
 array = [1, 4, 8, 2, -6, 11]
-p swap_elements(array)
+p swap_elements_from_to(array, 1, 4)
