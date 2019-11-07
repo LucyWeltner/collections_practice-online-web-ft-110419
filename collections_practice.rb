f9b20e892adaeaef478deb9089ef9c8528517e34
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:31:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,9 +4,10 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  sorted_array = array.sort
-  reversed_and_sorted = sorted_array.reverse 
-  reversed_and_sorted
+  sorted_array = array.sort{|a,b| a <=> b}
+  sorted_array
+  #reversed_and_sorted = sorted_array.reverse 
+  #reversed_and_sorted
 end
 
 array = [1, 4, 8, 2, -6, 11]
