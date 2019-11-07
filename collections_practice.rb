8857119365f0b3b6e0019368f0f2cdfdfef44477
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:36:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,9 +6,17 @@ end
 def sort_array_desc(array)
   sorted_array = array.sort{|a,b| b <=> a}
   sorted_array
-  #reversed_and_sorted = sorted_array.reverse 
-  #reversed_and_sorted
 end
 
+def sort_array_char_count(array)
+  sorted_array = array.sort{|a,b| a.length <=> b.length}
+  sorted_array
+end
+
+def swap_elements(array)
+  second_element = array[1]
+  third_element = array[2]
+  switched_array = Array.new
+  
 array = [1, 4, 8, 2, -6, 11]
 p sort_array_desc(array)
