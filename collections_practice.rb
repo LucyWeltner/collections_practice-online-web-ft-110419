c354a45ff86fe4188c4c0581db7b2c171a88f198
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:27:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,13 @@
+def sort_array_asc(array)
+  sorted_array = array.sort
+  sorted_array
+end
+
+def sort_array_desc
+  sorted_array = array.sort
+  reversed_and_sorted = sorted_array.reverse 
+  reversed_and_sorted
+end
+
 array = [1, 4, 8, 2, -6, 11]
-sorted_array = array.sort
-p sorted_array
+p sort_array_desc(array)
