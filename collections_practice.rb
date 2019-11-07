0c88648bed4b4affd858539f68b189f813ebf5ea
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-07 19:39:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,9 +14,11 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  second_element = array[1]
-  third_element = array[2]
   switched_array = Array.new
-  
+  switched_array[1] = array[2]
+  switched_array[2] = array[1]
+  switched_array = array[0] + 
+  switched_array.push(array[3..array.length])
+end
 array = [1, 4, 8, 2, -6, 11]
 p sort_array_desc(array)
